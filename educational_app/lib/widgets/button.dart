0536c0ebd770f446b7442a30cbf14e277795ea74
woacwoak <woacwoak@gmail.com>
2025-11-25 16:07:11 +0100

    I added icons for game buttons

--- educational_app/lib/widgets/button.dart
+++ educational_app/lib/widgets/button.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class GameButton extends StatelessWidget{
   final String text;
   final Color color;
-
+  final String image;
   final Widget gamePage;
 
   //Constructor
   const GameButton({
     super.key,
     required this.text,
+    required this.image,
     required this.color,
     required this.gamePage,
   });
@@ -34,13 +36,28 @@ class GameButton extends StatelessWidget{
             borderRadius: BorderRadius.circular(15),
           )
         ),
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 36, color: Colors.white, fontFamily: 'Itim'),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Text(
+                text,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 36, color: Colors.white, fontFamily: 'Itim'),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: SvgPicture.asset(
+                    'assets/icons/$image.svg',
+                    height: 40,
+                    width: 40,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
-
-
 }
