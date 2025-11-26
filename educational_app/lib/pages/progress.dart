951db5685d81ae3cee300dab65d2f0abd79442b7
woacwoak <woacwoak@gmail.com>
2025-11-26 14:45:37 +0100

    I added UI for Progress Page

--- educational_app/lib/pages/progress.dart
+++ educational_app/lib/pages/progress.dart
@@ -1,5 +1,7 @@
+import 'package:curious_bear/widgets/navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:curious_bear/widgets/custom_app_bar.dart';
+import 'package:flutter_svg/svg.dart';
 
 
 class ProgressPage extends StatelessWidget{
@@ -9,15 +11,83 @@ class ProgressPage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: appBar(context, "Progress"),
+      bottomNavigationBar: AppNavigationBar(),
       backgroundColor: Color(0xffFFF2D9),
-      body: Center(
-        child: Column(
-          children: [
-            
-          ],
-        )
-      ),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top:40),
+            child: Image.asset(
+              "assets/images/badge.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "Completed games",
+            style: TextStyle(
+            color: Color(0xff3A2A1E),
+            fontSize: 36,
+            fontWeight: FontWeight.normal,
+            fontFamily: 'Itim'
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: SizedBox(
+              width: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ProgressRow(path: "assets/icons/Math.svg", text: "5+ / 10- / 15"),
+                  SizedBox(height: 20),
+                  ProgressRow(path: "assets/icons/Brain.svg", text: "3+ / 2- / 5"),
+                  SizedBox(height: 20),
+                  ProgressRow(path: "assets/icons/Bulb.svg", text: "10+ / 0- / 10"),
+                ],
+              )
+            )
+          )
+        ],
+      )
     );
   }
 
+}
+
+class ProgressRow extends StatelessWidget{
+  final String path;
+  final String text;
+  
+  const ProgressRow({
+    super.key,
+    required this.path,
+    required this.text
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      children: [
+        SvgPicture.asset(
+          path,
+          height: 60,
+          width: 60,
+        ),
+        SizedBox(width: 20),
+        Text(
+          text,
+          style: TextStyle(
+            color: Color(0xff3A2A1E),
+            fontSize: 36,
+            fontWeight: FontWeight.normal,
+            fontFamily: 'Itim'
+          ),
+        ),
+        
+      ],
+    );
+  }
 }
--- educational_app/lib/widgets/navigation.dart
+++ educational_app/lib/widgets/navigation.dart
@@ -1,5 +1,6 @@
 import 'package:curious_bear/pages/games.dart';
 import 'package:curious_bear/pages/home.dart';
+import 'package:curious_bear/pages/progress.dart';
 import 'package:curious_bear/pages/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -62,7 +63,7 @@ class AppNavigationBar extends StatelessWidget {
               height: 40,
             ),
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const Placeholder()));
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const ProgressPage()));
             },
           ),
           label: '',
