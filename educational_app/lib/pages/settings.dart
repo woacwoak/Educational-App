15cb0152bf7d33fe64f887c5b0a80d97d6a460c1
woacwoak <woacwoak@gmail.com>
2025-11-26 13:19:36 +0100

    I created a settings page with sound and music mixers

--- educational_app/lib/pages/settings.dart
+++ educational_app/lib/pages/settings.dart
@@ -1,9 +1,26 @@
 import "package:curious_bear/widgets/navigation.dart";
 import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
 import "../widgets/custom_app_bar.dart";
+import "package:audioplayers/audioplayers.dart";
 
-class SettingsPage extends StatelessWidget {
+
+
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  AudioPlayer musicPlayer = AudioPlayer();
+  AudioPlayer sfxPlayer = AudioPlayer();
+
+  double musicVolume = 0.5;
+  double sfxVolume = 0.5;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +39,100 @@ class SettingsPage extends StatelessWidget {
               'assets/images/honeycomb.png',
               width: 400,
               fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Text(
+             "Sound effects",
+             style: TextStyle(
+              color: Color(0xff3A2A1E),
+              fontSize: 36,
+              fontWeight: FontWeight.normal,
+              fontFamily: 'Itim'
               ),
-              
-            )
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 35,
+                  height: 35,
+                  child: SvgPicture.asset(
+                  "assets/icons/music.svg",
+                  ),
+                ),
+                
+                Expanded(
+                  child: SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Color(0xff3A2A1E),
+                      inactiveTrackColor: Color(0xffBD9577),
+                      thumbColor: Color(0xff3A2A1E),
+                      overlayColor: Color(0x333A2A1E),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10),
+                      trackHeight: 5,
+                    ),
+                    child: Slider(
+                    value: musicVolume,
+                    onChanged: (value){
+                      setState((){
+                        musicVolume = value;
+                      });
+                      musicPlayer.setVolume(value);
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 35,
+                  height: 35,
+                  child: SvgPicture.asset(
+                    "assets/icons/sound.svg",
+                  ),
+                ),
+
+                
+                Expanded(
+                  child: SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Color(0xff3A2A1E),
+                      inactiveTrackColor: Color(0xffBD9577),
+                      thumbColor: Color(0xff3A2A1E),
+                      overlayColor: Color(0x333A2A1E),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10),
+                      trackHeight: 5,
+                    ),
+                    child: Slider(
+                      value: sfxVolume,
+                      onChanged: (value){
+                        setState((){
+                          sfxVolume = value;
+                        });
+                        sfxPlayer.setVolume(value);
+                      },
+                    ),
+                  )
+                  
+                  )
+                ],
+              )
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
