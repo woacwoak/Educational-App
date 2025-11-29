08ffe47215003444dd80dae23590d1272d0b2834
woacwoak <woacwoak@gmail.com>
2025-11-29 16:44:40 +0100

    I added flippable cards for memory game

--- educational_app/lib/pages/memory.dart
+++ educational_app/lib/pages/memory.dart
@@ -1,26 +1,52 @@
 import 'package:curious_bear/widgets/custom_app_bar.dart';
 import 'package:curious_bear/widgets/navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_flip_card/flutter_flip_card.dart';
 
-class MemoryPage extends StatelessWidget{
+class MemoryPage extends StatefulWidget{
   const MemoryPage({super.key});
-  
+
+  @override
+  State<MemoryPage> createState() => _MemoryPageState();
+}
+
+class _MemoryPageState extends State<MemoryPage>{
+  final List<FlipCardController> controllers = List.generate(12, (_) => FlipCardController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(context, "Memory"),
       bottomNavigationBar: AppNavigationBar(),
       backgroundColor: Color(0xffFFF2D9),
-      body: GridView.count(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+        child: GridView.count(
         crossAxisCount: 3,
+        childAspectRatio: 1,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
         children: List.generate(12, (index) {
           return Center(
-            child: Text(
-              "Item $index",
-              style: TextStyle(fontSize: 36, color: Colors.black, fontFamily: 'Itim'),
-            ),
-          );
-        }),
+            child: SizedBox(
+              height: 110,
+              width: 110,
+              child: FlipCard(
+                controller: controllers[index],
+                rotateSide: RotateSide.bottom,
+                onTapFlipping: true, //When enabled, the card will flip automatically when touched.
+                axis: FlipAxis.vertical,
+                frontWidget: Image.asset(
+                  'assets/images/card_front.png',
+                  fit: BoxFit.fitHeight),
+                backWidget: Image.asset(
+                  'assets/images/badge.png',
+                  fit: BoxFit.fitHeight)
+                ),
+              )
+            );
+          }),
+        ),
       ),
     );
   }
