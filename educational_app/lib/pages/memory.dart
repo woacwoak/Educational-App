0f8737ef3baa60042c1228c073afb375a06ac3e0
woacwoak <woacwoak@gmail.com>
2025-12-01 10:56:52 +0100

    I finished memory game with randomly generated cards and end game screen

--- educational_app/lib/pages/memory.dart
+++ educational_app/lib/pages/memory.dart
@@ -1,3 +1,5 @@
+import 'package:curious_bear/pages/games.dart';
+import 'package:curious_bear/pages/math.dart';
 import 'package:curious_bear/widgets/custom_app_bar.dart';
 import 'package:curious_bear/widgets/navigation.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +14,93 @@ class MemoryPage extends StatefulWidget{
 
 class _MemoryPageState extends State<MemoryPage>{
   final List<FlipCardController> controllers = List.generate(12, (_) => FlipCardController());
+  
+  int? firstIndex;
+  int? secondIndex;
+  bool wait = false;
+  List<bool> matched = List.filled(12, false);
+
+  late List<String> cardImages;
+
+  @override
+  void initState() {
+    super.initState();
+    _generateCards();
+  }
+
+  void _generateCards() {
+    List<String> fruits = [
+      "assets/images/blackberry.png",
+      "assets/images/blueberry.png",
+      "assets/images/cherries.png",
+      "assets/images/grape.png",
+      "assets/images/raspberry.png",
+      "assets/images/strawberry.png",
+    ];
+
+    cardImages = [...fruits, ...fruits];
+
+    cardImages.shuffle();
+  }
+  void _handleCardTap(int index) async {
+    if(wait) return;
+    if(matched[index]) return;
+    if(firstIndex == index) return;
+
+    controllers[index].flipcard();
+
+    if(firstIndex == null) {
+      firstIndex = index;
+      return;
+    }
+
+    
+
+      secondIndex = index;
+      wait = true;
+
+      await Future.delayed(const Duration(milliseconds: 700));
+
+      if(cardImages[firstIndex!] == cardImages[secondIndex!]){
+        matched[firstIndex!] = true;
+        matched[secondIndex!] = true;
+      }else{
+        await controllers[firstIndex!].flipcard();
+        await controllers[secondIndex!].flipcard();
+      }
+
+      firstIndex = null;
+      secondIndex = null;
+      wait = false;
+
+      if(matched.every((m) => m)){
+        _showWinDialog();
+      }
+    }
+  void _showWinDialog() {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("🎉 You Win!"),
+        content: Text("All pairs matched!"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GamePage()),
+              );
+              
+            },
+            child: Text("Play other games"),
+          )
+        ],
+      );
+    },
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,19 +120,28 @@ class _MemoryPageState extends State<MemoryPage>{
             child: SizedBox(
               height: 110,
               width: 110,
-              child: FlipCard(
-                controller: controllers[index],
-                rotateSide: RotateSide.bottom,
-                onTapFlipping: true, //When enabled, the card will flip automatically when touched.
-                axis: FlipAxis.vertical,
-                frontWidget: Image.asset(
-                  'assets/images/card_front.png',
-                  fit: BoxFit.fitHeight),
-                backWidget: Image.asset(
-                  'assets/images/badge.png',
-                  fit: BoxFit.fitHeight)
+              child: GestureDetector(
+                onTap: () => _handleCardTap(index),
+                child: FlipCard(
+                  controller: controllers[index],
+                  rotateSide: RotateSide.bottom,
+                  onTapFlipping: false,
+                  axis: FlipAxis.vertical,
+                  frontWidget: Image.asset(
+                    'assets/images/card_front.png',
+                    fit: BoxFit.fitHeight
+                    ),
+                  backWidget: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset(
+                      cardImages[index],
+                      fit: BoxFit.fitHeight
+                    ),
+                  ),
                 ),
               )
+              
+              ),
             );
           }),
         ),
